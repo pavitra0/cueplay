8f27f764daf8c3eff556b7bad4ba2af4b68d320b
Pavitra Rathod <rathodpavitra007@gmail.com>
2025-07-18 00:11:28 +0530

    api

--- actions/playerFetch.js
+++ actions/playerFetch.js
@@ -2,7 +2,7 @@ export async function fetchTMDBData(query) {
   try {
     const isServer = typeof window === 'undefined';
     const baseUrl = isServer
-      ? process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+      ? process.env.NEXT_PUBLIC_SITE_URL || 'https://cueplay.vercel.app'
       : '';
 
     const res = await fetch(`${baseUrl}/api/get-id?query=${encodeURIComponent(query)}`);
